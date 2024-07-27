b3521ea844d16e5c825958b1ea6bcc5032d3f4ee
Abhishekkanojiya3 <abhi67562@gmail.com>
2024-07-27 18:17:38 +0530

    Dashboard Section Complete

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,11 +1,51 @@
+import Image from 'next/image';
 import styles from './rightbar.module.css'
+import { MdPlayCircleFilled } from 'react-icons/md';
 
 const Rightbar = () => {
   return (
-    <div>
-      rightbar
+    <div className={styles.container}>
+     <div className={styles.item}>
+      <div className={styles.bgConatainer}>
+        <Image src= '/astro.png' alt = '' fill className={styles.bg}/>
+      </div>
+      <div className={styles.texts}>
+        <span className={styles.notification}>🔥 Available now</span>
+        <h3 className={styles.title}>
+          How to use the new version of admin Dashboard?
+        </h3>
+        <span className={styles.subtitile}>
+          Take 4 minutes to learn
+        </span>
+        <p className={styles.desc}>
+          hbeffi fewiufewuifewuhf fewiufewui fe fefewuifweuhifw 
+        </p>
+        <button className={styles.button}>
+          <MdPlayCircleFilled/>
+          Watch
+        </button>
+      </div>
+     </div>
+     <div className={styles.item}>
+      <div className={styles.texts}>
+      <span className={styles.notification}>🚀 Coming Soon</span>
+      <h3 className={styles.title}>
+          How to use the new version of admin Dashboard?
+        </h3>
+        <span className={styles.subtitile}>
+          Take 4 minutes to learn
+        </span>
+        <p className={styles.desc}>
+          hbeffi u ifewuifewhufieufifweuf fewiufewuifewuhf fewiufewui fe fefewuifweuhifw 
+        </p>
+        <button className={styles.button}>
+          <MdPlayCircleFilled/>
+          Watch
+        </button>
+      </div>
+
+     </div>
     </div>
   )
 }
-
-export default Rightbar
+export default Rightbar;
